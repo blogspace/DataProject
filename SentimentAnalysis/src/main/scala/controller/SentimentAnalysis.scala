60a449bc73c343e4b81c9d4f380c32c6b784d6de
blogspace <liuhao@jianbing.com>
2019-05-09 16:02:05 +0800

    情感分类

--- SentimentAnalysis/src/main/scala/controller/SentimentAnalysis.scala
+++ SentimentAnalysis/src/main/scala/controller/SentimentAnalysis.scala
@@ -10,7 +10,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 import utils.tokens
 
 /**
-  * 文本情感分析
+  * 文本情感分类
   * @author aloha
   */
 object SentimentAnalysis {
@@ -18,22 +18,25 @@ object SentimentAnalysis {
   def main(args: Array[String]): Unit = {
     //Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
     //Logger.getLogger("org.apache.jetty.server").setLevel(Level.OFF)
-    val conf = new SparkConf().setAppName("SentimentAnalysis").setMaster("local")
+    val conf = new SparkConf().setAppName("SentimentAnalysis").setMaster("local[*]")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
     sc.setLogLevel("ERROR")
 
     //1.加载数据
     import sqlContext.implicits._
-    val dataDF = sc.textFile("D:\\datatest\\user_emotion\\data_train.csv").filter(_.split("\t").size==4)
+    val srcRDD = sc.textFile("D:\\datatest\\user_emotion\\data_train.csv").filter(_.split("\t").size==4)
       .map(line=>{
           val col = line.split("\t",-1)
        RawDataRecord(col(3),tokens.anaylyzerWords(col(2)).toArray().mkString(" "))
-        }).toDF()
+        })
     //dataInput.select("category", "text").take(2).foreach(println)
+    val splits = srcRDD.randomSplit(Array(0.7, 0.3))
+    var trainingDF = splits(0).toDF()
+    var testDF = splits(1).toDF()
 
     var tokenizer = new Tokenizer().setInputCol("text").setOutputCol("words")
-    var wordsData = tokenizer.transform(dataDF)
+    var wordsData = tokenizer.transform(trainingDF)
 
     wordsData.select($"category",$"text",$"words").take(2)
 
@@ -49,67 +52,29 @@ object SentimentAnalysis {
     var trainDataRdd = rescaledData.select($"category",$"features").map {
       case Row(label: String, features: mlV) =>
         LabeledPoint(label.toDouble, Vectors.dense(features.toArray))
-    }
-    println("output4：")
-    trainDataRdd.show()
-//
-   val model = NaiveBayes.train(trainDataRdd, lambda = 1.0, modelType = "multinomial")
-//
+    }.rdd
+
+    val model = NaiveBayes.train(trainDataRdd, lambda = 1.0, modelType = "multinomial")
+
     //测试数据集，做同样的特征表示及格式转换
-//    var testwordsData = tokenizer.transform(testDF)
-//    var testfeaturizedData = hashingTF.transform(testwordsData)
-//    var testrescaledData = idfModel.transform(testfeaturizedData)
-//    var testDataRdd = testrescaledData.select($"category",$"features").map {
-//      case Row(label: String, features: mlV) =>
-//        LabeledPoint(label.toDouble, Vectors.dense(features.toArray))
-//    }
+    var testwordsData = tokenizer.transform(testDF)
+    var testfeaturizedData = hashingTF.transform(testwordsData)
+    var testrescaledData = idfModel.transform(testfeaturizedData)
+
+    var testDataRdd = testrescaledData.select($"category",$"features").map {
+      case Row(label: String, features: mlV) =>
+        LabeledPoint(label.toDouble, Vectors.dense(features.toArray))
+    }
 
     //对测试数据集使用训练模型进行分类预测
-//    val testpredictionAndLabel = testDataRdd.map(p => (model.predict(p.features), p.label))
-//
-//    //统计分类准确率
-//    var testaccuracy = 1.0 * testpredictionAndLabel.filter(x => x._1 == x._2).count() / testDataRdd.count()
-//    println("output5：")
-//    println(testaccuracy)
-    //2.计算词频矩阵
-//     val hashingTF = new HashingTF(numFeatures = 2000)
-//    val tf_feature = hashingTF.transform(dataInput)
-//    val tf_feature = dataInput.map(line => {
-//      (line._1,hashingTF.transform(line._2))
-//    }).foreach(println)
-//    val idf = new IDF()
-//    val idf_model = idf.fit(tf_feature)
-//    val tf_idf = tf_feature.map(line =>(line._1,idf_model.transform(line._2))).foreach(println)
-//    val Array(trainData, testData) = tf_idf.randomSplit(Array(0.9, 0.1), seed = 1234L)
-//    val naiveBayesModel = NaiveBayes.train(trainData,lambda = 1.0)
-    //val pre=testData.map(p=>(naiveBayesModel.predict(p.features),p.label))//验证模型
-//    val pre=test.map(p=>(naiveBayesModel.predict(p.features),p.label))//验证模型
-//    val prin=pre.take(20)
-    //Select(prediction,true label) and compute test error
-//    val evaluator = new MulticlassClassificationEvaluator()
-//      .setLabelCol("label")
-//      .setPredictionCol("prediction")
-//      .setMetricName("accuracy")
-//    val accuracy = evaluator.evaluate(predictions)
-//    println(s"Test set accuracy = $accuracy")
-//    logger.warn(TimeUtil.getLongCurrentTime() + " end!")
-
-    //4.分词
-    //    val data = sc.textFile("D:\\datatest\\user_emotion\\data_train.csv").map(x => {
-    //      val list = tokens.anaylyzerWords(x)
-    //      list.toString.split("\t")
-    //    }).flatMap(x => x.toList).map(x => (x.trim(), 1)).reduceByKey(_ + _).top(20)(Ord.reverse).foreach(println)
-    //  }
-    //
-    //  //分词排序
-    //  object Ord extends Ordering[(String, Int)] {
-    //    def compare(a: (String, Int), b: (String, Int)) = a._2 compare (b._2)
-    //  }
-
+    val testpredictionAndLabel = testDataRdd.map(p => (model.predict(p.features), p.label))
+    //统计分类准确率
+    var testaccuracy = 1.0 * testpredictionAndLabel.filter(x => x._1 == x._2).count() / testDataRdd.count()
+    println("output5：")
+    println(testaccuracy)
 
     sc.stop()
   }
-
 }
 
 
